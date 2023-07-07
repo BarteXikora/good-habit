9e983bb3d8723a55d846df1c7a796d5cda45c25b
BarteXikora <sbartek50@gmail.com>
2023-07-07 12:28:22 +0200

    Prepared pages components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import ProtectedRoutes from './functions/ProtectedRoutes'
+import PageHome from './components/pages/home/PageHome'
+import PageLogin from './components/pages/login/PageLogin'
+import Page404 from './components/pages/404/Page404'
 
 const App = () => {
   return <BrowserRouter>
@@ -7,12 +10,14 @@ const App = () => {
       <Route element={<ProtectedRoutes />}>
         <Route path='/' element={<Navigate to='/home' />} />
 
-        <Route path='/home' element={null} />
+        <Route path='/home' element={<PageHome />} />
       </Route>
 
-      <Route path='/login' element={null} />
+      <Route path='/login' element={<PageLogin />} />
 
-      <Route path='*' element={null} />
+      <Route path='/404' element={<Page404 />} />
+
+      <Route path='*' element={<Navigate to='/404' />} />
     </Routes>
   </BrowserRouter>
 }
