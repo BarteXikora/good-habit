be0d4a524f323d8aa952358b9d043824893e2c7c
BarteXikora <sbartek50@gmail.com>
2023-07-07 12:14:41 +0200

    Installed React Router and created basic router structure.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+
 const App = () => {
-  return <h1>Hello, World!</h1>
+  return <BrowserRouter>
+    <Routes>
+      <Route element={null}>
+        <Route path='/' element={<Navigate to='/home' />} />
+
+        <Route path='/home' element={null} />
+      </Route>
+
+      <Route path='/login' element={null} />
+
+      <Route path='*' element={null} />
+    </Routes>
+  </BrowserRouter>
 }
 
 export default App
